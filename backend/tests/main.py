8546961d8ea6bf3fc5b3288eebccca8c3bb55a1a
Konpeitou24 <161587218+Konpeitou24@users.noreply.github.com>
2025-07-17 22:26:54 +0900

    add: テスト関数のコンフリクト対策として、コードを残すことをコメント化した。
    notier_testを例としてインスタンス化し、テストを実行した。

--- backend/tests/main.py
+++ backend/tests/main.py
@@ -1,8 +1,14 @@
+from tests.notifier_test import NotifierTest
+
+
 class Main:
     def __init__(self):
         pass
 
 def main():
-    pass
+    # テストクラスのインスタンス化と、エントリポイント実行は残すこと。
+    notifier_test = NotifierTest()
+    notifier_test.run_tests()
+    
 if __name__ == "__main__":
     main()
