62fce94c998cae178fb84a5bba2152d3431e03c8
Konpeitou24 <161587218+Konpeitou24@users.noreply.github.com>
2025-08-19 09:00:58 +0900

    feat: ステータスに関する定義の追加

---
+++ frontend/DaraMemo/Enums/ActivityState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaraMemo.Enums {
+    /// <summary>
+    /// InSession 中の下位状態：作業中 or 離席中
+    /// </summary>
+    public enum ActivityState {
+        Active,
+        Afk
+    }
+}
